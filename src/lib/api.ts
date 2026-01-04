f8ecf4af2f2e79af075622d960dbdc84654f44e7
Erick Castelo Branco <erickcastelo2@gmail.com>
2026-01-04 11:52:18 -0400

    Added variable env

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 export const instance = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 instance.interceptors.request.use(
